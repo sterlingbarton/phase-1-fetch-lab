921990ac246bfb9fb3badac16db8f40d98afee8b
Sterling <sterlingbarton81@gmail.com>
2023-03-27 05:27:08 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(function(data){
+    renderBooks(data);
+  })
+  return fetch('https://anapioficeandfire.com/api/books');
 }
 
 function renderBooks(books) {
